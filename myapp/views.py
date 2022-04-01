6df13a6b4fc39e54db6dd274d89d546b570c713e
Roberto Evans <evans.joewono@gmail.com>
2022-04-01 13:42:08 +0700

    Django Models

--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 def index(request):
-    context = {
-        'name': 'Patrick',
-        'age': 23,
-        'nationality': 'Indonesian'
-    }
-    return  render(request, 'index.html',context)
+    # for index_old.html
+    # context = {
+    #     'name': 'Patrick',
+    #     'age': 23,
+    #     'nationality': 'Indonesian'
+    # }
+
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.details = 'Our serivce is OP'
+
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'Mega Fast'
+    feature2.details = 'Our serivce is Mega OP'
+
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'Mega Fast'
+    feature3.details = 'Our serivce is Mega OP'
+
+    feature4 = Feature()
+    feature4.id = 3
+    feature4.name = 'Mega Fast'
+    feature4.details = 'Our serivce is Mega OP'
+
+    feature5 = Feature()
+    feature5.id = 4
+    feature5.name = 'Mega Fast'
+    feature5.details = 'Our serivce is Mega OP'
+
+    features = [feature1, feature2, feature3, feature4, feature5]
+
+    # for index_old.html
+    # return  render(request, 'index.html',context)
+
+    return render(request, 'index.html', {'features': features})
 
 def counter(request):
     words = request.POST['words'] ##Must have POST if using form method POST. GET if GET
