628270aeab7ee0a224b603633f102ec2b3f90e17
Roberto Evans <evans.joewono@gmail.com>
2022-04-05 14:19:52 +0700

    Admin Panel use

--- myapp/views.py
+++ myapp/views.py
@@ -10,36 +10,43 @@ def index(request):
     #     'age': 23,
     #     'nationality': 'Indonesian'
     # }
-
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name = 'Fast'
-    feature1.details = 'Our serivce is OP'
-
-    feature2 = Feature()
-    feature2.id = 1
-    feature2.name = 'Mega Fast'
-    feature2.details = 'Our serivce is Mega OP'
-
-    feature3 = Feature()
-    feature3.id = 2
-    feature3.name = 'Mega Fast'
-    feature3.details = 'Our serivce is Mega OP'
-
-    feature4 = Feature()
-    feature4.id = 3
-    feature4.name = 'Mega Fast'
-    feature4.details = 'Our serivce is Mega OP'
-
-    feature5 = Feature()
-    feature5.id = 4
-    feature5.name = 'Mega Fast'
-    feature5.details = 'Our serivce is Mega OP'
-
-    features = [feature1, feature2, feature3, feature4, feature5]
-
-    # for index_old.html
     # return  render(request, 'index.html',context)
+
+    # For manual input. Superseded by using Django Models
+    # feature1 = Feature()
+    # feature1.id = 0
+    # feature1.name = 'Fast'
+    # feature1.is_true = True
+    # feature1.details = 'Our serivce is OP'
+
+    # feature2 = Feature()
+    # feature2.id = 1
+    # feature2.name = 'Mega Fast'
+    # feature2.is_true = True
+    # feature2.details = 'Our serivce is Mega OP'
+
+    # feature3 = Feature()
+    # feature3.id = 2
+    # feature3.name = 'Mega Fast'
+    # feature3.is_true = True
+    # feature3.details = 'Our serivce is Mega OP'
+
+    # feature4 = Feature()
+    # feature4.id = 3
+    # feature4.name = 'Mega Fast'
+    # feature4.is_true = False
+    # feature4.details = 'Our serivce is Mega OP'
+
+    # feature5 = Feature()
+    # feature5.id = 4
+    # feature5.name = 'Mega Fast'
+    # feature5.is_true = True
+    # feature5.details = 'Our serivce is Mega OP'
+
+    # features = [feature1, feature2, feature3, feature4, feature5]
+
+    # Get all objects from Feature -> all the data like name and details!
+    features= Feature.objects.all()
 
     return render(request, 'index.html', {'features': features})
 
