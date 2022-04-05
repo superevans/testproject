10622ff23d3d1a29f8c45904e239f1eafefc36c3
Roberto Evans <evans.joewono@gmail.com>
2022-04-05 15:43:19 +0700

    User Registration

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature
 
 # Create your views here.
@@ -54,3 +56,30 @@ def counter(request):
     words = request.POST['words'] ##Must have POST if using form method POST. GET if GET
     amount_of_words = len(words.split())
     return render(request, 'counter.html', {'amount': amount_of_words})
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists(): #Filter the DB to check if the email is already registered
+                messages.info(request, 'Email already used!') #Requests is used to send response
+                return redirect('register')
+            elif User.objects.filter(username=username).exists(): #Filter the DB to check if the username is already registered
+                messages.info(request, 'Username already used!') #Requests is used to send response
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save();
+                return redirect('login')
+        else:
+            messages.info(request, 'Password not the same!') #Requests is used to send response
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+        
+
+    
